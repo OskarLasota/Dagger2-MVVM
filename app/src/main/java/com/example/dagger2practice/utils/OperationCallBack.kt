c1eb28761200126f2c9575b15e79c3f505519772
Oskar Lasota <55768328+OskarLasota@users.noreply.github.com>
2020-06-15 19:26:40 +0100

    initial commit

--- app/src/main/java/com/example/dagger2practice/utils/OperationCallBack.kt
+++
@@ -1,8 +0,0 @@
-package com.example.dagger2practice.utils
-
-interface OperationCallBack<T> {
-
-    //onsuccess needs to be made generic somehow
-    fun onSuccess(data: ArrayList<T>)
-    fun onError(error:String?)
-}
