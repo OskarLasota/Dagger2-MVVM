0a14493c02e34154eceb6fe255b7926da98e6d0b
Oskar Lasota <55768328+OskarLasota@users.noreply.github.com>
2020-06-15 19:47:36 +0100

    chore: set up mockito testing

--- Dagger2-MVVM/app/src/test/java/com/frezzcoding/dagger2_mvvm/ExampleUnitTest.kt
+++ Dagger2-MVVM/app/src/test/java/com/frezzcoding/dagger2_mvvm/ExampleUnitTest.kt
@@ -1,17 +1,60 @@
 package com.frezzcoding.dagger2_mvvm
 
+import com.frezzcoding.dagger2_mvvm.models.Product
+import com.frezzcoding.dagger2_mvvm.repo.ProductRepository
+import com.frezzcoding.dagger2_mvvm.utils.OperationCallBack
+import com.frezzcoding.dagger2_mvvm.vm.ProductsViewModel
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.runners.MockitoJUnitRunner
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@RunWith(MockitoJUnitRunner::class)
 class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+
+
+
+    lateinit var viewModel : ProductsViewModel
+
+    @Mock
+    lateinit var callback : OperationCallBack<Product>
+
+    @Mock
+    lateinit var service : ProductService
+
+    lateinit var repo : ProductRepository
+
+
+
+
+    @Before
+    fun setUp(){
+        repo = ProductRepository(service)
+        viewModel = ProductsViewModel(repo)
     }
+
+    @Test
+    fun `on obtaining products, is service called`(){
+        viewModel.getProducts()
+        Mockito.verify(service, Mockito.atLeastOnce()).products()
+    }
+
+    @Test
+    fun `on obtaining products, loading is true`() {
+        viewModel.getProducts()
+        println(viewModel.isViewLoading.value)
+        assert(viewModel.isViewLoading.value!!)
+    }
+
+
+
 }
